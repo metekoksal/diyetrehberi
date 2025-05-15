f2c1cd8fe75e8ce8121171e0c25c3f87e31ce510
metekoksal <mehmetmetekoksal@hotmail.com>
2025-05-16 01:03:30 +0300

    gelecek düzenlemeler için hafifletildi

--- src/main/java/diyetrehberi/diyetrehberi/ExerciseEntry.java
+++ src/main/java/diyetrehberi/diyetrehberi/ExerciseEntry.java
@@ -1,70 +1,17 @@
 package diyetrehberi.diyetrehberi;
 
-import java.sql.Time;
+import java.sql.*;
 
-public class ExerciseEntry implements Trackable {
-    private int id;
-    private String name;
+public class ExerciseEntry extends Entry {
     private int duration;
-    private double calories;
-    private Time timeDone;
 
-    // Time exercise was done
-    public Time getTime(){
-        return timeDone;
-    }
     // Calories Burned (-)
     public double calculateCalories(){
-        return -calories;
-    }
-
-    // Constructor
-    public ExerciseEntry(int id, String name, int duration, double calories, Time timeDone) {
-        this.id = id;
-        this.name = name;
-        this.duration = duration;
-        this.calories = calories;
-        this.timeDone = timeDone;
+        return -getCalories();
     }
 
     // Getters & Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getDuration() {
         return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public double getCalories() {
-        return calories;
-    }
-
-    public void setCalories(double calories) {
-        this.calories = calories;
-    }
-
-    public Time getTimeDone() {
-        return timeDone;
-    }
-
-    public void setTimeDone(Time timeDone) {
-        this.timeDone = timeDone;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 }
