139a741d8985ce3987335b985b55f4d1cecc06a2
metekoksal <mehmetmetekoksal@hotmail.com>
2025-05-16 01:00:17 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module diyetrehberi.diyetrehberi {
 
     requires eu.hansolo.tilesfx;
     requires java.sql;
+    requires jdk.unsupported.desktop;
 
     opens diyetrehberi.diyetrehberi to javafx.fxml;
     exports diyetrehberi.diyetrehberi;
