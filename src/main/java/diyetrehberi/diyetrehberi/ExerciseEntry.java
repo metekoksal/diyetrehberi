850ce4d4f5d378db22f04754c2de326d46796e97
metekoksal <mehmetmetekoksal@hotmail.com>
2025-05-17 16:53:08 +0300

    egzersiz kaydı için metodlar eklendi

--- src/main/java/diyetrehberi/diyetrehberi/ExerciseEntry.java
+++ src/main/java/diyetrehberi/diyetrehberi/ExerciseEntry.java
@@ -3,15 +3,48 @@ package diyetrehberi.diyetrehberi;
 import java.sql.*;
 
 public class ExerciseEntry extends Entry {
-    private int duration;
+    private int duration;   //minutes
 
     // Calories Burned (-)
     public double calculateCalories(){
         return -getCalories();
     }
 
-    // Getters & Setters
+    public ExerciseEntry(int id, int duration) {
+        super(id);
+        this.duration = duration;
+        try {
+            loadExerciseDataFromDatabase(id, duration);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private void loadExerciseDataFromDatabase(int id, int duration) throws SQLException {
+        Connection connection = Database.getInstance().getConnection();
+
+        String sql = "SELECT name, calories_burned_per_min, category FROM exercises WHERE exercise_id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+
+                    this.setName(rs.getString("name"));
+                    this.setCalories(duration * rs.getDouble("calories_burned_per_min"));
+                    this.setCategory(rs.getString("category"));
+                } else {
+                    throw new SQLException("Exercise with ID " + id + " not found");
+                }
+            }
+        }
+    }
+
     public int getDuration() {
         return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 }
