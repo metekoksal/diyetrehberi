f6b80a4c0df452eb793b6baa48e574b2257385c5
metekoksal <mehmetmetekoksal@hotmail.com>
2025-05-18 19:02:47 +0300

    Ana uygulama classı ismi düzeltildi

--- src/main/java/diyetrehberi/diyetrehberi/MainApplication.java
+++ src/main/java/diyetrehberi/diyetrehberi/MainApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Database db = Database.getInstance();
