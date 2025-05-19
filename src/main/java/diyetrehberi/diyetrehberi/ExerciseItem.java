6894e30e04b05b128398c2efe8a07fc5df1ebf63
metekoksal <mehmetmetekoksal@hotmail.com>
2025-05-19 17:39:55 +0300

    Yeni kayıt yapısı için constructorlar ve variable lar düzenlendi

--- src/main/java/diyetrehberi/diyetrehberi/ExerciseItem.java
+++ src/main/java/diyetrehberi/diyetrehberi/ExerciseItem.java
@@ -2,13 +2,15 @@ package diyetrehberi.diyetrehberi;
 
 public class ExerciseItem {
     private String name;
-    private String category; // in minutes
+    private String category;
+    private int exerciseId;
     private double caloriesBurnedPerMin;
 
-    public ExerciseItem(String Name, String Category, double CaloriesBurnedPerMin) {
+    public ExerciseItem(String Name, String Category, double CaloriesBurnedPerMin, int ExerciseId) {
         this.name = Name;
         this.category = Category;
         this.caloriesBurnedPerMin = CaloriesBurnedPerMin;
+        this.exerciseId = ExerciseId;
     }
 
     public String getName() {
@@ -21,5 +23,29 @@ public class ExerciseItem {
 
     public double getCaloriesBurnedPerMinute() {
         return caloriesBurnedPerMin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(int exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public double getCaloriesBurnedPerMin() {
+        return caloriesBurnedPerMin;
+    }
+
+    public void setCaloriesBurnedPerMin(double caloriesBurnedPerMin) {
+        this.caloriesBurnedPerMin = caloriesBurnedPerMin;
     }
 }
--- src/main/java/diyetrehberi/diyetrehberi/FoodItem.java
+++ src/main/java/diyetrehberi/diyetrehberi/FoodItem.java
@@ -1,15 +1,75 @@
 package diyetrehberi.diyetrehberi;
 
 public class FoodItem {
-    String name;
-    double porsiyon, kalori, protein, karbonhidrat, yag;
+    private String name, kategori;
+    private int foodid;
+    private double porsiyon, kalori, protein, karbonhidrat, yag;
 
-    public FoodItem(String name, double porsiyon, double kalori, double protein, double karbonhidrat, double yag) {
+    public FoodItem(String name, double porsiyon, double kalori, double protein, double karbonhidrat, double yag, String category, int foodid) {
         this.name = name;
         this.porsiyon = porsiyon;
         this.kalori = kalori;
         this.protein = protein;
         this.karbonhidrat = karbonhidrat;
+        this.yag = yag;
+        this.kategori = category;
+        this.foodid = foodid;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFoodid() {
+        return foodid;
+    }
+
+    public void setFoodid(int foodid) {
+        this.foodid = foodid;
+    }
+
+    public double getPorsiyon() {
+        return porsiyon;
+    }
+
+    public void setPorsiyon(double porsiyon) {
+        this.porsiyon = porsiyon;
+    }
+
+    public double getKalori() {
+        return kalori;
+    }
+
+    public void setKalori(double kalori) {
+        this.kalori = kalori;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getKarbonhidrat() {
+        return karbonhidrat;
+    }
+
+    public void setKarbonhidrat(double karbonhidrat) {
+        this.karbonhidrat = karbonhidrat;
+    }
+
+    public double getYag() {
+        return yag;
+    }
+
+    public void setYag(double yag) {
         this.yag = yag;
     }
 }
